ab7c6e2e76f6548dd07adc3b59b01a917d5e70df
sergej <sergej@abyss.fritz.box>
2016-12-30 00:09:55 +0100

    Add function to drop every k element in a list

--- lists/lists.scala
+++ lists/lists.scala
@@ -12,3 +12,16 @@ def duplicate2[T](list: List[T]): List[T] = {
 
 def duplicateN[T](count: Int, list: List[T]) =
   list.flatMap(e => List.fill(count)(e))
+
+def drop1[T](position: Int, list: List[T]) =
+  ((1 to list.size) zip list).withFilter( _._1 % position != 0).map(_._2)
+
+def drop2[T](position: Int, list: List[T]) = {
+  def loop(list: List[T], acc: List[T], index: Int): List[T] =
+    list match {
+      case x :: xs => loop(xs, if (index % position == 0) acc else x :: acc, index + 1)
+      case _ => acc.reverse
+    }
+
+  loop(list, Nil, 1)
+}
