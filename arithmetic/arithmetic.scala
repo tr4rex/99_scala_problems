21e961247416d3c782508a8fb0514604f092cc75
craig_tucker <sergej.shinkarenko@phystech.edu>
2018-04-23 23:32:42 +0200

    Add few more arithmetic functions

--- arithmetic/arithmetic.scala
+++ arithmetic/arithmetic.scala
@@ -1,3 +1,5 @@
+import scala.language.postfixOps
+
 def isPrime(x: Int) = {
   import scala.math.sqrt
   x > 1 && (2 to sqrt(x).floor.toInt).forall(x % _ != 0)
@@ -20,4 +22,33 @@ def primeFactors(n: Int) = {
   loop(n, primes, Nil).reverse
 }
 
-def primeFactorMultiplicity(n: Int) = primeFactors(n).groupBy(x => x).map { case (k, v) => (k, v.size) }
+def primeFactorMultiplicity(n: Int): Map[Int, Int] = 
+  primeFactors(n).groupBy(x => x).map { case (k, v) => (k, v.size) }
+
+def pow(x: Int, y: Int): Int =
+  y match {
+    case 0 => 1
+    case 1 => x
+    case p if p % 2 == 0 => pow(x * x, p / 2)
+    case p if p % 2 == 1 => x * pow(x, p - 1)
+  }
+
+def totientImproved(x: Int): Int =
+  primeFactorMultiplicity(x).
+    map { case(k, v) => ((k - 1) * pow(k, v - 1)).toInt }.
+    product
+
+def functionComparison[I, T](f1: I => T, f2: I => T)(v: I): (Long, Long) = {
+  def measure[I, T](f: I => T)(v: I): Long = {
+    val start = System.currentTimeMillis()
+    f(v)
+    val end = System.currentTimeMillis()
+    end - start
+  }
+
+  (measure(f1)(v), measure(f2)(v))
+}
+
+def primesRange(x: Int, y: Int): List[Int] = primes takeWhile(_ <= y) dropWhile(_ < x) toList
+
+def goldbach(x: Int): (Int, Int) = primes.find(p => isPrime(x - p)).map(p => (p, x - p)).head
